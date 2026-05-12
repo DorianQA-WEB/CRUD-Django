f0b6dda4e729be95412ea89b3b0780b662566d34
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-05-12 16:06:20 +0500

    docstr

--- crud/forms.py
+++ crud/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm, CharField, IntegerField
 from .models import Person
 
+
+# Форма для создания и редактирования пользователя
 class PersonForm(ModelForm):
     name = CharField(label="Имя пользователя",)
     age = IntegerField(label="Введите возраст")
